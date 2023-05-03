88d1f04b006c0e0c5ca8cf51bada58073f73e136
Robert Hogan <robhogan@gmail.com>
2023-05-03 20:28:38 +0100

    Add missing palette file

---
+++ src/palettes.asm
@@ -0,0 +1,91 @@
+
+paletteArrayLoPtr
+.BYTE <palette1,<palette2,<palette3,<palette4,<palette5,<palette6,<palette7,<palette8
+.BYTE <palette9,<palette10,<palette11,<palette12,<palette13,<palette14,<palette15,<palette16
+
+paletteArrayHiPtr
+.BYTE >palette1,>palette2,>palette3,>palette4,>palette5,>palette6,>palette7,>palette8
+.BYTE >palette9,>palette10,>palette11,>palette12,>palette13,>palette14,>palette15,>palette16
+
+; Background Palettes
+palette1
+.BYTE $0F,$00,$10,$20
+.BYTE $0F,$10,$10,$10
+.BYTE $0F,$20,$20,$20
+.BYTE $0F,$30,$20,$30
+palette2
+.BYTE $0F,$11,$21,$31
+.BYTE $0F,$11,$21,$21
+.BYTE $0F,$11,$21,$11
+.BYTE $0F,$11,$21,$31
+palette3
+.BYTE $0F,$12,$22,$32
+.BYTE $0F,$12,$22,$32
+.BYTE $0F,$12,$22,$32
+.BYTE $0F,$12,$22,$32
+palette4
+.BYTE $0F,$13,$23,$33
+.BYTE $0F,$13,$23,$33
+.BYTE $0F,$13,$23,$33
+.BYTE $0F,$13,$23,$33
+palette5
+.BYTE $0F,$14,$24,$34
+.BYTE $0F,$14,$24,$34
+.BYTE $0F,$14,$24,$34
+.BYTE $0F,$14,$24,$34
+palette6
+.BYTE $0F,$15,$25,$35
+.BYTE $0F,$15,$25,$35
+.BYTE $0F,$15,$25,$35
+.BYTE $0F,$15,$25,$35
+palette7
+.BYTE $0F,$16,$26,$36
+.BYTE $0F,$16,$26,$36
+.BYTE $0F,$16,$26,$36
+.BYTE $0F,$16,$26,$36
+palette8
+.BYTE $0F,$17,$27,$37
+.BYTE $0F,$17,$27,$37
+.BYTE $0F,$17,$27,$37
+.BYTE $0F,$17,$27,$37
+palette9
+.BYTE $0F,$18,$28,$38
+.BYTE $0F,$18,$28,$38
+.BYTE $0F,$18,$28,$38
+.BYTE $0F,$18,$28,$38
+palette10
+.BYTE $0F,$19,$29,$39
+.BYTE $0F,$19,$29,$39
+.BYTE $0F,$19,$29,$39
+.BYTE $0F,$19,$29,$39
+palette11
+.BYTE $0F,$1A,$2A,$3A
+.BYTE $0F,$1A,$2A,$3A
+.BYTE $0F,$1A,$2A,$3A
+.BYTE $0F,$1A,$2A,$3A
+palette12
+.BYTE $0F,$1B,$2B,$3B
+.BYTE $0F,$1B,$2B,$3B
+.BYTE $0F,$1B,$2B,$3B
+.BYTE $0F,$1B,$2B,$3B
+palette13
+.BYTE $0F,$1C,$2C,$3C
+.BYTE $0F,$1C,$2C,$3C
+.BYTE $0F,$1C,$2C,$3C
+.BYTE $0F,$1C,$2C,$3C
+palette14
+.BYTE $0F,$1D,$2D,$3D
+.BYTE $0F,$1D,$2D,$3D
+.BYTE $0F,$1D,$2D,$3D
+.BYTE $0F,$1D,$2D,$3D
+palette15
+.BYTE $0F,$1E,$2E,$3E
+.BYTE $0F,$1E,$2E,$3E
+.BYTE $0F,$1E,$2E,$3E
+.BYTE $0F,$1E,$2E,$3E
+palette16
+.BYTE $0F,$1F,$2F,$3F
+.BYTE $0F,$1F,$2F,$3F
+.BYTE $0F,$1F,$2F,$3F
+.BYTE $0F,$1F,$2F,$3F
+
